9cad2a44637c1c2ac77cf931d325c48b3cf4cea6
Rasputin <medina.engineering@outlook.com>
2020-03-11 03:57:48 -0400

    +Implementing Basic Movement
    Basic Player Movement

--- ShipGame/Assets/Scripts/Player.cs
+++ ShipGame/Assets/Scripts/Player.cs
@@ -6,9 +6,18 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _cannon01Position;
+    [SerializeField]
+    private float _fireRate = 0.15f;
 
+    private float _canFire = -1f;
     private float _horizontalAxis = 0f;
     private float _verticalAxis = 0f;
+
+    
 
 
     // Start is called before the first frame update
@@ -21,6 +30,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         this.CalculateMovement();
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > _canFire)
+        {
+            this.Fire();
+        }
+
     }
 
     private void CalculateMovement()
@@ -38,19 +53,14 @@ public class Player : MonoBehaviour
         else if (this.transform.position.x <= -11.15f)
         {
             this.transform.position = new Vector3(11.05f, this.transform.position.y, this.transform.position.z);
-        }
-
-        //this.transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, ))
-
-        //if (this.transform.position.y <= -3.2f)
-        //{
-        //    this.transform.position = new Vector3(this.transform.position.x, -3.2f, this.transform.position.z);
-        //}
-        //else if (this.transform.position.y >= 0)
-        //{
-        //    this.transform.position = new Vector3(this.transform.position.x, 0f, this.transform.position.z);
-        //}
+        }      
 
         this.transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.2f, 0), 0);
+    }
+
+    private void Fire()
+    {        
+         _canFire = Time.time + _fireRate;
+         Instantiate(_laserPrefab, _cannon01Position.transform.position, Quaternion.identity);        
     }
 }
---
+++ ShipGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    private float _laserSpeed = 8.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        transform.Translate(Vector3.up * _laserSpeed * Time.deltaTime);
+
+        if(transform.position.y >= 8.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
